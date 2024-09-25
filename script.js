025882ec1673fd9d5317526e4be532b6481fe6be
lucas_demaimay <demaimay.lucas@gmail.com>
2024-09-25 22:35:15 +0200

    alternance travail et repos + bouton reset

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let affichage = document.getElementById("affichageTemps");
 let controle = document.getElementById("boutonControle");
 
 const minuteur = { 
-    minute : 1, 
+    minute : 25, 
     seconde : 0,
-    tabTravailRepos: [25, 0, 1, 1],
+    tabTravailRepos: [25, 0, 5, 0],
     estTempsPause: false,
     estActif: false,
 
@@ -21,9 +21,11 @@ const minuteur = {
                 if(this.estTempsPause) {
                     this.minute = this.tabTravailRepos[0];
                     this.seconde = this.tabTravailRepos[1];
+                    this.estTempsPause = false;
                 } else {
                     this.minute = this.tabTravailRepos[2];
                     this.seconde = this.tabTravailRepos[3];
+                    this.estTempsPause = true;
                 }
             }
         }
@@ -31,8 +33,9 @@ const minuteur = {
     },
 
     restaurer: function() {
-        this.minute = tabTravailRepos[0];
-        this.seconde = tabTravailRepos[1]; 
+        this.estTempsPause = false;
+        this.minute = this.tabTravailRepos[0];
+        this.seconde = this.tabTravailRepos[1]; 
         this.reafficher();
     },
 
@@ -42,10 +45,18 @@ const minuteur = {
         } else {
             affichage.textContent = this.minute + " : " + this.seconde;
         }
+
+        if(this.estTempsPause){
+            document.body.style.backgroundColor = "GREEN";
+        } else {
+            document.body.style.backgroundColor = "RED";
+        }
+
+        if(!this.estActif){
+            document.body.style.backgroundColor = "WHITE";
+        }
     }
 }
-
-
 
 affichage.textContent = minuteur.minute + " : 0" + minuteur.seconde;
 
@@ -53,5 +64,17 @@ setInterval(() => minuteur.anvancer(), 1000);
 
 function lancer(){
     minuteur.estActif = true;
+    document.body.style.backgroundColor = "RED";
+    controle.textContent = "Reset";
+    controle.onclick = reset;
+}
+
+function reset(){
+    minuteur.estActif = false;
+
+    minuteur.restaurer();
+
+    controle.textContent = "Lancer";
+    controle.onclick = lancer;
 }
 
