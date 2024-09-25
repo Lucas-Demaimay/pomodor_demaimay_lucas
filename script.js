a3b953fafa02af166636622fee7dfce53767a441
lucas_demaimay <demaimay.lucas@gmail.com>
2024-09-25 21:39:03 +0200

    lancement du minuteur fonctionnel

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
 
-let seconde = 0;
-let minute = 25;
-
 let affichage = document.getElementById("affichageTemps");
-affichage.textContent = minute + " " + seconde;
+
+let controle = document.getElementById("boutonControle");
+
+const minuteur = { 
+    minute : 1, 
+    seconde : 0,
+    tabTravailRepos: [25, 0, 1, 1],
+    estTempsPause: false,
+    estActif: false,
+
+    anvancer: function() {
+        if(!this.estActif) return;
+        this.seconde -= 1;
+        if(this.seconde == -1){
+            this.seconde = 59;
+            
+            this.minute -= 1;
+            if(this.minute == -1) {
+                if(this.estTempsPause) {
+                    this.minute = this.tabTravailRepos[0];
+                    this.seconde = this.tabTravailRepos[1];
+                } else {
+                    this.minute = this.tabTravailRepos[2];
+                    this.seconde = this.tabTravailRepos[3];
+                }
+            }
+        }
+        this.reafficher();
+    },
+
+    restaurer: function() {
+        this.minute = tabTravailRepos[0];
+        this.seconde = tabTravailRepos[1]; 
+        this.reafficher();
+    },
+
+    reafficher: function() {
+        if(this.seconde < 10){
+            affichage.textContent = this.minute + " : 0" + this.seconde;
+        } else {
+            affichage.textContent = this.minute + " : " + this.seconde;
+        }
+    }
+}
+
+
+
+affichage.textContent = minuteur.minute + " : 0" + minuteur.seconde;
+
+setInterval(() => minuteur.anvancer(), 1000);
+
+function lancer(){
+    minuteur.estActif = true;
+}
 
